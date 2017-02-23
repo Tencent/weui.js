9f1895f79c4817bf07c754637360f871c1e99861
bearyan <bearverygood@126.com>
2017-02-23 16:03:10 +0800

    优化picker的id的注释

--- src/picker/picker.js
+++ src/picker/picker.js
@@ -24,7 +24,7 @@ let temp = {}; // temp 存在上一次滑动的位置
  * @param {array} items picker的数据，即用于生成picker的数据，picker的层级可以自己定义，但建议最多三层。数据格式参考example。
  * @param {Object} options 配置项
  * @param {number=} [options.depth] picker深度(也就是picker有多少列) 取值为1-3。如果为空，则取items第一项的深度。
- * @param {string=} [options.id=default] 作为picker的唯一标识
+ * @param {string=} [options.id=default] 作为picker的唯一标识，作用是以id缓存当时的选择。（当你想每次传入的defaultValue都是不一样时，可以使用不同的id区分）
  * @param {string=} [options.className] 自定义类名
  * @param {array=} [options.defaultValue] 默认选项的value数组
  * @param {function=} [options.onChange] 在picker选中的值发生变化的时候回调

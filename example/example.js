c3e1617dc3bef3b6d4d73957c030457c68951912
bearyan <bearverygood@126.com>
2017-05-03 22:09:19 +0800

    fix example手动上传图片不显示的问题 #116

--- example/example.js
+++ example/example.js
@@ -491,7 +491,7 @@ document.querySelector('#uploaderCustomFiles').addEventListener('click', functio
     var id = target.getAttribute('data-id');
 
     if(url){
-        url = url.match(/url\((.*?)\)/)[1];
+        url = url.match(/url\((.*?)\)/)[1].replace(/"/g, '');
     }
     var gallery = weui.gallery(url, {
         onDelete: function(){

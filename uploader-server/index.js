c0d0091f319fb8cc6103b89200f2b2b616fd8f63
Robert <464554492@qq.com>
2017-05-24 10:18:14 +0800

    自动创建存储图片的文件夹
    自动创建存储图片的文件夹

--- uploader-server/index.js
+++ uploader-server/index.js
@@ -14,21 +14,30 @@ var allowCrossDomain = function(req, res, next) {
 };
 app.use(allowCrossDomain);
 
+
+function upload_path(path) {
+    var _path = `${__dirname}/${path}`;
+    try {
+        fs.accessSync(_path);
+    } catch (e) {
+        fs.mkdirSync(_path);
+    }
+    return `${_path}/`;
+}
+
 function node_upload(req, callback, next) {
     var form = new formidable.IncomingForm();
     form.encoding = 'utf-8';
-    form.uploadDir = __dirname + '/uploads/';
+    form.uploadDir = upload_path('uploads');
     form.maxFieldsSize = 10 * 1024 * 1024;
     //解析
-    form.parse(req, function (err, fields, files) {
-        if(err) return callback(err);
+    form.parse(req, function(err, fields, files) {
+        if (err) return callback(err);
 
         for (var file in files) {
             //后缀名
             var extName = /\.[^\.]+/.exec(files[file].name);
-            var ext = Array.isArray(extName)
-                ? extName[0]
-                : '';
+            var ext = Array.isArray(extName) ? extName[0] : '';
             //重命名，以防文件重复
             var avatarName = uuid() + ext;
             //移动的文件目录
@@ -46,13 +55,13 @@ function node_upload(req, callback, next) {
     });
 }
 app.post('/upload', function(req, res, next) {
-    node_upload(req, function (err, fields) {
-        // setTimeout(function(){
+    node_upload(req, function(err, fields) {
+        setTimeout(function() {
             res.json(fields);
-        // }, 1000);
+        }, 1000);
     }, next);
 });
 
-http.createServer(app).listen(8002, function(){
+http.createServer(app).listen(8002, function() {
     console.log('Express server listening on port 8002');
 });

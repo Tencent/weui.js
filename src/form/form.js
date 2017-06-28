a5033983bc12b7220f85c3a8132fc607624ee852
bearyan <bearverygood@126.com>
2017-06-28 11:49:44 +0800

    优化 支持checkbox的reg是{0, x}的情况 #102

--- src/form/form.js
+++ src/form/form.js
@@ -1,13 +1,13 @@
 /*
 * Tencent is pleased to support the open source community by making WeUI.js available.
-* 
+*
 * Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
-* 
+*
 * Licensed under the MIT License (the "License"); you may not use this file except in compliance
 * with the License. You may obtain a copy of the License at
-* 
+*
 *       http://opensource.org/licenses/MIT
-* 
+*
 * Unless required by applicable law or agreed to in writing, software distributed under the License is
 * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 * either express or implied. See the License for the specific language governing permissions and
@@ -48,19 +48,17 @@ function _validate($input, $form, regexp){
                     if(checkboxInput.checked) ++count;
                 });
 
-                if(!count) return 'empty';
-
                 if(regs[1] === ''){ // {0,}
                     if(count >= parseInt(regs[0])){
                         return null;
                     }else{
-                        return 'notMatch';
+                        return count == 0 ? 'empty' : 'notMatch';
                     }
                 }else{ // {0,2}
                     if(parseInt(regs[0]) <= count && count <= parseInt(regs[1])){
                         return null;
                     }else{
-                        return 'notMatch';
+                        return count == 0 ? 'empty' : 'notMatch';
                     }
                 }
             }else{

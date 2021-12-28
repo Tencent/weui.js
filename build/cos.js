c64af54da7d1e5de8f58f82b13da78d12a328e99
bearyan <bearyan@tencent.com>
2021-12-28 20:59:43 +0800

    改用execSync

--- build/cos.js
+++ build/cos.js
@@ -1,6 +1,6 @@
 /* 配合蓝盾发布到腾讯CDN */
 
 const pkg = require('../package.json');
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 
-exec(`mkdir dist/${pkg.version} & mv dist/weui.js dist/weui.min.js dist/${pkg.version}`);
+execSync(`mkdir -p dist/cos/${pkg.version} & mv dist/weui.js dist/weui.min.js dist/cos/${pkg.version}`);

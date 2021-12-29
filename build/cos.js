4c58f0f2b499c921ca09b653e2e3bdaaf516c25c
bearyan <bearyan@tencent.com>
2021-12-29 18:02:51 +0800

    fix: 偶现创建文件夹太慢导致找不到文件夹的问题

--- build/cos.js
+++ build/cos.js
@@ -3,4 +3,5 @@
 const pkg = require('../package.json');
 const { execSync } = require('child_process');
 
-execSync(`mkdir -p dist/cos/${pkg.version} & mv dist/weui.js dist/weui.min.js dist/cos/${pkg.version}`);
+execSync(`mkdir -p dist/cos/${pkg.version}`);
+execSync(`mv dist/weui.js dist/weui.min.js dist/cos/${pkg.version}`);
